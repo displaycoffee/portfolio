d68260496bdff5bb612c6d67c4800bb4c1e2ddc8
displaycoffee <adria.m.murphy@gmail.com>
2023-09-14 10:16:11 -0600

    started portfolio

---
+++ assets/src/js/pages/Index.jsx
@@ -0,0 +1,19 @@
+/* React */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+/* Local scripts */
+// none
+
+/* Local components */
+// none
+
+export const Index = () => {
+	return (
+		<div className="wrapper">
+			stuff - <a href="">stufff</a>
+			<img src="./assets/dist/images/favicon-32x32.png" alt="" />
+			<img src="./assets/dist/images/mug-blue-36x36.png" alt="" />
+		</div>
+	);
+};
