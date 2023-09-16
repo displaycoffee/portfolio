a82d5c05f2ddafe5120c1508756bf48e4864ae5c
displaycoffee <adria.m.murphy@gmail.com>
2023-09-16 10:07:28 -0600

    fix path

--- assets/src/js/scripts/elements.js
+++ assets/src/js/scripts/elements.js
@@ -1,6 +1,3 @@
-/* Local scripts */
-import { variables } from '../scripts/variables';
-
 /* Local components */
 import { About } from '../pages/About';
 import { Art } from '../pages/Art';
@@ -15,7 +12,7 @@ export const elements = {
 		{
 			id: 7,
 			label: 'Start',
-			path: variables.paths.base,
+			path: '/',
 			component: (props) => {
 				return props ? <Start props={props} /> : <Start />; 
 			}
