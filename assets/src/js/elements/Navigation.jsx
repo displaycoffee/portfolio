6bb6e65357694823629c08c657fb1be1d7e05ea5
displaycoffee <adria.m.murphy@gmail.com>
2023-09-26 17:41:11 -0600

    fix styles, functionality, and add about page

--- assets/src/js/elements/Navigation.jsx
+++ assets/src/js/elements/Navigation.jsx
@@ -5,13 +5,29 @@ import { useState } from 'react';
 /* Local scripts */
 import { elements } from '../scripts/elements';
 
-export const Navigation = () => {
+export const Navigation = (props) => {
+	const { toggleMenu } = props;
+
 	return elements.navigation && elements.navigation.length != 0 ? (
 		<nav className="navigation">
 			<ul className="navigation-list unstyled">
 				{elements.navigation.map((navigation) => (
 					<li className="navigation-list-item" key={navigation.id}>
-						<Link to={navigation.path}>{navigation.label}</Link>
+						<Link
+							to={navigation.url}
+							onClick={(e) => {
+								// If in mobile menu, close menu with timeout after using link
+								if (toggleMenu) {
+									setTimeout(() => {
+										toggleMenu(e);
+									}, 100);
+								}
+							}}
+							alt={navigation.alt || navigation.label}
+							title={navigation.alt || navigation.label}
+						>
+							{navigation.label}
+						</Link>
 					</li>
 				))}
 			</ul>
@@ -22,12 +38,12 @@ export const Navigation = () => {
 export const NavigationMenu = () => {
 	let [menu, setMenu] = useState(false);
 	const navigationActiveClass = 'navigation-menu-active';
-	
+
 	// Toggle menu
 	const toggleMenu = (e) => {
 		e.preventDefault();
 		const bodySelector = document.querySelector('body');
-		
+
 		// Change menu state and body class
 		if (menu) {
 			menu = false;
@@ -36,7 +52,7 @@ export const NavigationMenu = () => {
 			menu = true;
 			bodySelector.classList.add(navigationActiveClass);
 		}
-		
+
 		// Set menu state
 		setMenu(menu);
 	};
@@ -53,10 +69,12 @@ export const NavigationMenu = () => {
 				<header className="navigation-menu-header flex-nowrap flex-align-items-center">
 					<h3 className="navigation-menu-title">Menu</h3>
 
-					<button className="navigation-menu-close" type="button" onClick={(e) => toggleMenu(e)}>x</button>
+					<button className="navigation-menu-close" type="button" onClick={(e) => toggleMenu(e)}>
+						x
+					</button>
 				</header>
 
-				<Navigation />
+				<Navigation toggleMenu={toggleMenu} />
 			</div>
 		</div>
 	);
--- assets/src/js/scripts/utils.js
+++ assets/src/js/scripts/utils.js
@@ -2,11 +2,19 @@
 import { useState } from 'react';
 
 export const utils = {
-	respond : (bp) => {
+	handleize: (value) => {
+		// Format value for html classes
+		return value
+			.toLowerCase()
+			.replace(/[^\w\s]/g, '')
+			.replace(/\s/g, '-')
+			.trim();
+	},
+	respond: (bp) => {
 		const rule = window.matchMedia(`(min-width: ${bp}px)`);
 		let [match, setMatch] = useState(rule.matches);
-		
-		// update match state on media change
+
+		// Update match state on media change
 		rule.onchange = (e) => {
 			if (e.matches) {
 				match = true;
@@ -17,5 +25,5 @@ export const utils = {
 		};
 
 		return match;
-	}
+	},
 };
