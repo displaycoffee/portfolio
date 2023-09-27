70c2d39d161646b93b5356254e1b3ee8016512ea
displaycoffee <adria.m.murphy@gmail.com>
2023-09-27 13:25:45 -0600

    primarily resume addition

--- assets/src/js/elements/Header.jsx
+++ assets/src/js/elements/Header.jsx
@@ -1,3 +1,6 @@
+/* React */
+import { Link } from 'react-router-dom';
+
 /* Local components */
 import { NavigationMenu } from '../elements/Navigation';
 
@@ -8,15 +11,17 @@ export const Header = (props) => {
 	return (
 		<header className="header spacing-reset">
 			<h1 className="header-title">
-				<span className="header-type">{`* { display : coffee; }`}</span>
+				<Link to={'/'} alt="Back to home" title="Back to home">
+					<span className="header-type">{`* { display : coffee; }`}</span>
+				</Link>
 			</h1>
 
 			<div className="header-mugs">
-				{mugs.map((mug) => (
-					<img src={`./assets/dist/images/theme/mug-${mug}-${isDesktop ? '36x36' : '18x18'}.png`} alt={`${mug} coffee mug`} key={mug} />
+				{mugs.map((mug, index) => (
+					<img src={`./assets/dist/images/theme/mug-${mug}-${isDesktop ? '36x36' : '18x18'}.png`} alt={`${mug} coffee mug`} key={index} />
 				))}
 			</div>
-			
+
 			{!isDesktop && (
 				<div className="header-navigation">
 					<NavigationMenu />
--- assets/src/js/pages/Resume.jsx
+++ assets/src/js/pages/Resume.jsx
@@ -1,5 +1,96 @@
+/* React */
+import React from 'react';
+
+/* Local scripts */
+import { pages } from '../scripts/pages';
+
 export const Resume = () => {
+	const { technical, history } = pages.resume;
+	const { languages, cms, tools, other } = technical;
+
 	return (
-		<p>A better resume will be coming soon. In the meantime, download a <a href="./assets/dist/documents/adria-murphy-resume.pdf" target="_blank">boring pdf here</a>.</p>
+		<>
+			<p>
+				<strong>Note</strong> - If you're looking for a downloadable version of this resume, grab the pdf{' '}
+				<a href="./assets/dist/documents/adria-murphy-resume.pdf" target="_blank">
+					here
+				</a>
+				.
+			</p>
+
+			<h4>Skills</h4>
+
+			{languages && languages.length !== 0
+				? languages.map((language, index) => <ResumeSkillBar key={index} name={language.name} outOf={language.value} />)
+				: null}
+
+			<h4>Inventory</h4>
+
+			<ul>
+				{cms && (
+					<li>
+						<strong>Armor</strong> - {cms}
+					</li>
+				)}
+				{tools && (
+					<li>
+						<strong>Weapons</strong> - {tools}
+					</li>
+				)}
+				{other && (
+					<li>
+						<strong>Items</strong> - {other}
+					</li>
+				)}
+			</ul>
+
+			<h4>Quest Log</h4>
+
+			{history && history.length !== 0
+				? history.map((career, index) => (
+						<React.Fragment key={index}>
+							<p>
+								<strong>Name</strong> - {career.name}
+							</p>
+
+							<p>
+								<strong>Role</strong> - <span dangerouslySetInnerHTML={{ __html: career.role }}></span>
+							</p>
+
+							<p>
+								<strong>Objectives</strong> -
+							</p>
+
+							<ul>
+								{career.content.map((content, index) => (
+									<li key={index}>
+										{index == career.content.length - 1 ? (
+											<>
+												<strong>Inventory</strong> -&nbsp;
+											</>
+										) : null}
+										{content}
+									</li>
+								))}
+							</ul>
+						</React.Fragment>
+				  ))
+				: null}
+		</>
+	);
+};
+
+export const ResumeSkillBar = (props) => {
+	const { name, outOf } = props;
+
+	return (
+		<div className="skill-bar flex-wrap flex-align-items-center">
+			<div className="skill-bar-name">{name}</div>
+
+			<div className="skill-bar-progress pixel-border">
+				<div className="skill-bar-total"></div>
+				<div className="skill-bar-out-of" style={{ width: `${outOf * 10}%` }}></div>
+			</div>
+		</div>
 	);
 };
