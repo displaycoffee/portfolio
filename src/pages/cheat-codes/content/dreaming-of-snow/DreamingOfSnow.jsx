94fa90e513ae35cbe6d16ed9507215b1a1538f85
displaycoffee <adria.m.murphy@gmail.com>
2025-02-15 16:05:24 -0700

    update sass calcs, add new template

--- src/pages/cheat-codes/content/dreaming-of-snow/DreamingOfSnow.jsx
+++ src/pages/cheat-codes/content/dreaming-of-snow/DreamingOfSnow.jsx
@@ -39,6 +39,13 @@ export const DreamingOfSnow = () => {
 					</a>
 				</p>
 
+				<h5 className="h-remove-shadow">HTML or React</h5>
+				<p>
+					The avatar be altered by changing the below <CodeInline>img</CodeInline> source. If possible, please re-host any images and upload
+					to your own server.
+				</p>
+				<CodeBlock header={'Images'}>{cb01}</CodeBlock>
+
 				<h5 className="h-remove-shadow">HTML</h5>
 				<p>Copy the template HTML and place it on the page.</p>
 				<p>
@@ -62,8 +69,8 @@ export const DreamingOfSnow = () => {
 						dreaming-of-snow/DreamingOfSnow.jsx
 					</a>
 				</p>
-				<CodeBlock header={'React import'}>{cb01}</CodeBlock>
-				<CodeBlock header={'React component'}>{cb02}</CodeBlock>
+				<CodeBlock header={'React import'}>{cb02}</CodeBlock>
+				<CodeBlock header={'React component'}>{cb03}</CodeBlock>
 
 				<h5 className="h-remove-shadow">CSS or Sass</h5>
 				<p>
@@ -73,12 +80,12 @@ export const DreamingOfSnow = () => {
 					</a>{' '}
 					and are imported into the CSS files. You can change the fonts to whatever you'd like.
 				</p>
-				<CodeBlock header={'Fonts'}>{cb03}</CodeBlock>
+				<CodeBlock header={'Fonts'}>{cb04}</CodeBlock>
 				<p>
 					The image is a background image in the styles and can be altered by updating the below url. If possible, please re-host any images
 					and upload to your own server.
 				</p>
-				<CodeBlock header={'Images'}>{cb04}</CodeBlock>
+				<CodeBlock header={'Images'}>{cb05}</CodeBlock>
 
 				<h5 className="h-remove-shadow">CSS</h5>
 				<p>If using CSS and not Sass, copy the styles from the link below and add according to your preferred method.</p>
@@ -91,14 +98,14 @@ export const DreamingOfSnow = () => {
 						dreaming-of-snow/dreaming-of-snow.css
 					</a>
 				</p>
-				<CodeBlock header={'Style block'}>{cb05}</CodeBlock>
-				<CodeBlock header={'Stylesheet'}>{cb06}</CodeBlock>
-				<CodeBlock header={'CSS import'}>{cb07}</CodeBlock>
-				<CodeBlock header={'React import'}>{cb08}</CodeBlock>
+				<CodeBlock header={'Style block'}>{cb06}</CodeBlock>
+				<CodeBlock header={'Stylesheet'}>{cb07}</CodeBlock>
+				<CodeBlock header={'CSS import'}>{cb08}</CodeBlock>
+				<CodeBlock header={'React import'}>{cb09}</CodeBlock>
 				<p>
 					To change the <CodeInline>max-width</CodeInline>, look for the following code at the 768px breakpoint rule:
 				</p>
-				<CodeBlock header={'CSS'}>{cb09}</CodeBlock>
+				<CodeBlock header={'CSS'}>{cb10}</CodeBlock>
 
 				<h5 className="h-remove-shadow">Sass</h5>
 				<p>If using Sass and not CSS, copy the styles from the link below and add according to your preferred method.</p>
@@ -111,12 +118,12 @@ export const DreamingOfSnow = () => {
 						dreaming-of-snow/dreaming-of-snow.scss
 					</a>
 				</p>
-				<CodeBlock header={'Sass import'}>{cb10}</CodeBlock>
-				<CodeBlock header={'React import'}>{cb11}</CodeBlock>
+				<CodeBlock header={'Sass import'}>{cb11}</CodeBlock>
+				<CodeBlock header={'React import'}>{cb12}</CodeBlock>
 				<p>
 					There is a variable configuration to change the <CodeInline>max-width</CodeInline> of the template:
 				</p>
-				<CodeBlock header={'Sass'}>{cb12}</CodeBlock>
+				<CodeBlock header={'Sass'}>{cb13}</CodeBlock>
 			</CheatCodesSection>
 
 			<Preview className="preview-cheat-codes preview-dreaming-of-snow">
@@ -129,77 +136,103 @@ export const DreamingOfSnow = () => {
 export const DreamingOfSnowPreview = () => {
 	return (
 		<div className="dc-dreaming-of-snow displaycoffee">
-			<div className="dc-dreaming-of-snow-banner">
-				<h3 className="dc-dreaming-of-snow-title">Wish you were here...</h3>
-				<div className="dc-dreaming-of-snow-circles"></div>
-			</div>
-			<div className="dc-dreaming-of-snow-content">
+			<header className="dc-dreaming-of-snow-header">
 				<div className="dc-dreaming-of-snow-row">
-					<div className="dc-dreaming-of-snow-column dc-dreaming-of-snow-column-01">
-						<dl>
-							<dt>Name:</dt>
-							<dd>Beach Goer</dd>
-							<dt>Gender:</dt>
-							<dd>Unknown</dd>
-							<dt>Location:</dt>
-							<dd>Beachy Beachtown</dd>
-							<dt>Occupation:</dt>
-							<dd>Beach Bum</dd>
-							<dt>Favorite Drink:</dt>
-							<dd>Pineapple juice and vodka</dd>
-							<dt>Favorite Song:</dt>
-							<dd>Kokomo</dd>
-							<dt>Another Field:</dt>
-							<dd>Another answer</dd>
-							<dt>Another Field:</dt>
-							<dd>Another answer</dd>
-							<dt>Another Field:</dt>
-							<dd>Another answer</dd>
-						</dl>
+					<div className="dc-dreaming-of-snow-column-title dc-dreaming-of-snow-column">
+						<h3 className="dc-dreaming-of-snow-title dc-dreaming-of-snow-blue">
+							I'm dreaming of snow,
+							<br />
+							Lorem ipsum dolor sit amet...
+						</h3>
+					</div>
+
+					<div className="dc-dreaming-of-snow-column-avatar dc-dreaming-of-snow-column">
+						<div className="dc-dreaming-of-snow-avatar">
+							<div className="dc-dreaming-of-snow-image-wrapper">
+								<img
+									src="//display.coffee/assets/images/cheat-codes/dreaming-of-snow-avatar.jpg"
+									alt="Dreaming of Snow - Avatar"
+									title="Dreaming of Snow - Avatar"
+								/>
+							</div>
+
+							<div className="dc-dreaming-of-snow-blue">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt interdum vulputate.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<main className="dc-dreaming-of-snow-content">
+				<div className="dc-dreaming-of-snow-row">
+					<div className="dc-dreaming-of-snow-column">
 						<p>
-							<a href="/">Link 01</a>
-							<span className="dc-dreaming-of-snow-separator"></span>
-							<a href="/">Link 02</a>
-							<span className="dc-dreaming-of-snow-separator"></span>
-							<a href="/">Link 03</a>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor rutrum velit in porttitor. Integer imperdiet est
+							et sem pretium, at dignissim nibh consequat. Suspendisse ex sapien, vestibulum eget mollis eu, facilisis nec nisl. Nulla
+							facilisi.
+						</p>
+						<p>
+							Praesent sollicitudin massa quis ultricies viverra. Duis sagittis neque dolor, ut gravida eros volutpat et. Curabitur
+							tincidunt <a href="/">fringilla velit</a>, ut cursus erat. Cras ut euismod est, et dictum erat. Nam velit tellus, molestie
+							sed nunc id, ultricies euismod massa. In sed sem quis lorem euismod euismod. Mauris eget nulla ultricies, tempus felis a,
+							porta ipsum. Aliquam at arcu tellus.
+						</p>
+						<p>
+							Nulla urna nisi, vulputate sit amet nisi ac, mollis eleifend nunc. Cum sociis natoque penatibus et magnis dis parturient
+							montes, nascetur ridiculus mus. Nam auctor commodo purus, id dapibus erat viverra sit amet. Praesent{' '}
+							<a href="/">et metus</a> vel erat porta gravida sed sed libero.
 						</p>
 					</div>
-					<div className="dc-dreaming-of-snow-column dc-dreaming-of-snow-column-02">
-						<h4 className="dc-dreaming-of-snow-title">Biography</h4>
+
+					<div className="dc-dreaming-of-snow-column">
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis est quam accusantium! Ut corporis consequatur,{' '}
-							<a href="/">nisi assumenda veniam</a>, necessitatibus. Obcaecati porro optio fuga quod itaque, quibusdam odit qui nobis.
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor rutrum velit in porttitor. Integer imperdiet est
+							et sem pretium, at dignissim nibh consequat. Suspendisse ex sapien, vestibulum eget mollis eu, facilisis nec nisl. Nulla
+							facilisi.
 						</p>
 						<p>
-							Neque possimus ipsam quas, corporis vitae at illo, ullam culpa non tempora dicta modi quibusdam saepe ipsa corrupti.{' '}
-							<a href="/">Quae</a> aliquid fuga reiciendis ipsam suscipit ex, nobis maiores excepturi, tempora. Ab.
+							Praesent sollicitudin massa quis ultricies viverra. Duis sagittis neque dolor, ut gravida eros volutpat et. Curabitur
+							tincidunt fringilla velit, ut cursus erat. Cras ut euismod est, et dictum erat. Nam velit tellus, molestie sed nunc id,
+							ultricies euismod massa. In sed sem quis lorem euismod euismod. Mauris <a href="/">eget nulla ultricies</a>, tempus felis
+							a, porta ipsum. Aliquam at arcu tellus.
 						</p>
 						<p>
-							Recusandae a <a href="/">ipsum</a> impedit laborum fugiat nihil esse quis libero hic quidem sapiente delectus cum aliquid
-							laudantium suscipit dolore, <a href="/">consectetur dolor</a> incidunt nam ipsa voluptas eius sunt mollitia. Sit, animi!
+							Nulla urna nisi, vulputate sit amet nisi ac, mollis eleifend nunc. Cum sociis natoque penatibus et magnis dis parturient
+							montes, nascetur ridiculus mus. Nam auctor commodo purus, id dapibus erat viverra sit amet. Praesent et metus vel erat
+							porta gravida sed sed libero.
 						</p>
 					</div>
 				</div>
-			</div>
+			</main>
+
+			<footer className="dc-dreaming-of-snow-footer dc-dreaming-of-snow-blue">
+				<h4>
+					Nulla urna nisi, vulputate sit amet nisi ac,
+					<br />
+					Nam auctor commodo purus, id dapibus erat viverra sit amet.
+				</h4>
+			</footer>
 		</div>
 	);
 };
 
 /* Code blocks */
-const cb01 = `import { DreamingOfSnow } from './DreamingOfSnow';`;
-const cb02 = `<DreamingOfSnow />`;
-const cb03 = `@import url('//fonts.googleapis.com/css2?family=Damion&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');`;
-const cb04 = `background: url('//display.coffee/assets/images/cheat-codes/dreaming-of-snow.jpg') no-repeat;`;
-const cb05 = `<style>
+const cb01 = `<img src="//display.coffee/assets/images/cheat-codes/dreaming-of-snow-avatar.jpg" alt="Dreaming of Snow - Avatar" title="Dreaming of Snow - Avatar" />`;
+const cb02 = `import { DreamingOfSnow } from './DreamingOfSnow';`;
+const cb03 = `<DreamingOfSnow />`;
+const cb04 = `@import url('//fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Vollkorn:ital,wght@0,400..900;1,400..900&display=swap');`;
+const cb05 = `background: url('//display.coffee/assets/images/cheat-codes/dreaming-of-snow-bg.jpg') no-repeat;`;
+const cb06 = `<style>
 	/* Copied styles go here */
 </style>`;
-const cb06 = `<!-- Copy styles and paste into stylesheet -->
+const cb07 = `<!-- Copy styles and paste into stylesheet -->
 <link rel="stylesheet" href="dreaming-of-snow.css" />`;
-const cb07 = `@import url('dreaming-of-snow.css');`;
-const cb08 = `import 'dreaming-of-snow.css';`;
-const cb09 = `.dc-dreaming-of-snow {
-	max-width: 650px;
+const cb08 = `@import url('dreaming-of-snow.css');`;
+const cb09 = `import 'dreaming-of-snow.css';`;
+const cb10 = `.dc-dreaming-of-snow {
+	max-width: 768px;
 }`;
-const cb10 = `@import 'dreaming-of-snow.scss';`;
-const cb11 = `import 'dreaming-of-snow.scss';`;
-const cb12 = `$dreaming-of-snow-max-width: 650px; // max width`;
+const cb11 = `@import 'dreaming-of-snow.scss';`;
+const cb12 = `import 'dreaming-of-snow.scss';`;
+const cb13 = `$dreaming-of-snow-max-width: 768px; // max width`;
