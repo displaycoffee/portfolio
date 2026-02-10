fec41f4a6060af6215681bf734a80fc43552b652
displaycoffee <adria.m.murphy@gmail.com>
2026-02-10 07:36:08 -0700

    fixing articles

--- src/pages/articles/scripts/articles-types.d.ts
+++ src/pages/articles/scripts/articles-types.d.ts
@@ -1,4 +1,11 @@
+/* Local scripts */
+import { ContentsType } from '../../../components/contents/scripts/contents-types';
+
 /* Type definitions */
+type ArticlesLocation = {
+	location: string;
+};
+
 type ArticlesSection = {
 	children: ReactNode;
 	header: string;
@@ -11,6 +18,10 @@ type ArticlesToc = {
 };
 
 /* Export prop types */
+export type ArticlesIndexProps = ArticlesLocation;
+
+export type ArticlesContentProps = ArticlesLocation;
+
 export type ArticlesSectionProps = ArticlesSection;
 
 export type ArticlesTocProps = ArticlesToc;
